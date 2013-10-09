35c9236cf808ba5761dc3677ea4595bed986c166
jwclark <jwclark@rockhursths.edu>
2013-10-09 16:00:07 -0500

    first working version of yearbook photos

--- Yearbook Photos/Yearbook Photos/Main.Designer.cs
+++ Yearbook Photos/Yearbook Photos/Main.Designer.cs
@@ -27,16 +27,22 @@
             this.lvCurrent_Last = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lvCurrent_First = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lvCurrent_Grade = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvCurrent_ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lvExport = new System.Windows.Forms.ListView();
             this.lvExport_Last = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lvExport_First = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lvExport_Grade = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvExport_ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.btnExport = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
-            this.pnlImage = new System.Windows.Forms.Panel();
             this.lblStub = new System.Windows.Forms.Label();
             this.comboYear = new System.Windows.Forms.ComboBox();
+            this.pnlImage = new System.Windows.Forms.PictureBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearchMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlImage)).BeginInit();
             this.SuspendLayout();
             // 
             // lvCurrent
@@ -44,35 +50,47 @@
             this.lvCurrent.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.lvCurrent_Last,
             this.lvCurrent_First,
-            this.lvCurrent_Grade});
-            this.lvCurrent.Location = new System.Drawing.Point(12, 148);
+            this.lvCurrent_Grade,
+            this.lvCurrent_ID});
+            this.lvCurrent.FullRowSelect = true;
+            this.lvCurrent.Location = new System.Drawing.Point(12, 112);
             this.lvCurrent.Name = "lvCurrent";
-            this.lvCurrent.Size = new System.Drawing.Size(175, 364);
+            this.lvCurrent.Size = new System.Drawing.Size(250, 400);
             this.lvCurrent.TabIndex = 0;
             this.lvCurrent.UseCompatibleStateImageBehavior = false;
             this.lvCurrent.View = System.Windows.Forms.View.Details;
+            this.lvCurrent.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.lvCurrent_ColumnClick);
+            this.lvCurrent.SelectedIndexChanged += new System.EventHandler(this.lvCurrent_SelectedIndexChanged);
             // 
             // lvCurrent_Last
             // 
             this.lvCurrent_Last.Text = "Last";
+            this.lvCurrent_Last.Width = 100;
             // 
             // lvCurrent_First
             // 
             this.lvCurrent_First.Text = "First";
+            this.lvCurrent_First.Width = 100;
             // 
             // lvCurrent_Grade
             // 
             this.lvCurrent_Grade.Text = "Grade";
+            // 
+            // lvCurrent_ID
+            // 
+            this.lvCurrent_ID.Text = "ID";
             // 
             // lvExport
             // 
             this.lvExport.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.lvExport_Last,
             this.lvExport_First,
-            this.lvExport_Grade});
-            this.lvExport.Location = new System.Drawing.Point(597, 12);
+            this.lvExport_Grade,
+            this.lvExport_ID});
+            this.lvExport.FullRowSelect = true;
+            this.lvExport.Location = new System.Drawing.Point(522, 112);
             this.lvExport.Name = "lvExport";
-            this.lvExport.Size = new System.Drawing.Size(175, 400);
+            this.lvExport.Size = new System.Drawing.Size(250, 400);
             this.lvExport.TabIndex = 1;
             this.lvExport.UseCompatibleStateImageBehavior = false;
             this.lvExport.View = System.Windows.Forms.View.Details;
@@ -80,48 +98,50 @@
             // lvExport_Last
             // 
             this.lvExport_Last.Text = "Last";
+            this.lvExport_Last.Width = 100;
             // 
             // lvExport_First
             // 
             this.lvExport_First.Text = "First";
+            this.lvExport_First.Width = 100;
             // 
             // lvExport_Grade
             // 
             this.lvExport_Grade.Text = "Grade";
             // 
+            // lvExport_ID
+            // 
+            this.lvExport_ID.Text = "ID";
+            // 
             // btnExport
             // 
-            this.btnExport.Location = new System.Drawing.Point(597, 419);
+            this.btnExport.Location = new System.Drawing.Point(697, 83);
             this.btnExport.Name = "btnExport";
             this.btnExport.Size = new System.Drawing.Size(75, 23);
             this.btnExport.TabIndex = 2;
             this.btnExport.Text = "Export";
             this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(309, 45);
+            this.btnAdd.Location = new System.Drawing.Point(268, 112);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(75, 23);
             this.btnAdd.TabIndex = 3;
-            this.btnAdd.Text = "Add";
+            this.btnAdd.Text = "Add >>";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(516, 13);
+            this.btnRemove.Location = new System.Drawing.Point(441, 112);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(75, 23);
             this.btnRemove.TabIndex = 4;
-            this.btnRemove.Text = "Remove";
+            this.btnRemove.Text = "<< Remove";
             this.btnRemove.UseVisualStyleBackColor = true;
-            // 
-            // pnlImage
-            // 
-            this.pnlImage.Location = new System.Drawing.Point(309, 112);
-            this.pnlImage.Name = "pnlImage";
-            this.pnlImage.Size = new System.Drawing.Size(172, 228);
-            this.pnlImage.TabIndex = 5;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // lblStub
             // 
@@ -140,14 +160,53 @@
             this.comboYear.Size = new System.Drawing.Size(121, 21);
             this.comboYear.TabIndex = 7;
             // 
+            // pnlImage
+            // 
+            this.pnlImage.Location = new System.Drawing.Point(274, 176);
+            this.pnlImage.Name = "pnlImage";
+            this.pnlImage.Size = new System.Drawing.Size(235, 311);
+            this.pnlImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pnlImage.TabIndex = 8;
+            this.pnlImage.TabStop = false;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(10, 69);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(44, 13);
+            this.lblSearch.TabIndex = 9;
+            this.lblSearch.Text = "Search:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 85);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(250, 20);
+            this.txtSearch.TabIndex = 10;
+            this.txtSearch.Click += new System.EventHandler(this.txtSearch_Click);
+            this.txtSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSearch_KeyPress);
+            // 
+            // lblSearchMessage
+            // 
+            this.lblSearchMessage.AutoSize = true;
+            this.lblSearchMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblSearchMessage.Location = new System.Drawing.Point(61, 69);
+            this.lblSearchMessage.Name = "lblSearchMessage";
+            this.lblSearchMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblSearchMessage.TabIndex = 11;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.lblSearchMessage);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.pnlImage);
             this.Controls.Add(this.comboYear);
             this.Controls.Add(this.lblStub);
-            this.Controls.Add(this.pnlImage);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnExport);
@@ -156,6 +215,7 @@
             this.Name = "Main";
             this.Text = "Yearbook Photos";
             this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pnlImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,9 +234,14 @@
         private System.Windows.Forms.Button btnExport;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnRemove;
-        private System.Windows.Forms.Panel pnlImage;
         private System.Windows.Forms.Label lblStub;
         private System.Windows.Forms.ComboBox comboYear;
+        private System.Windows.Forms.ColumnHeader lvCurrent_ID;
+        private System.Windows.Forms.ColumnHeader lvExport_ID;
+        private System.Windows.Forms.PictureBox pnlImage;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearchMessage;
     }
 }
 
--- Yearbook Photos/Yearbook Photos/Main.cs
+++ Yearbook Photos/Yearbook Photos/Main.cs
@@ -11,8 +11,13 @@ using System.Windows.Forms;
 namespace Yearbook_Photos {
     public partial class Main : Form {
 
+        private const string ROOT_PATH = @"C:\LocalStorage\static";
+        private ListViewColumnSorter sorter;
+
         public Main() {
             InitializeComponent();
+            sorter = new ListViewColumnSorter();
+            lvCurrent.ListViewItemSorter = sorter;
         }
 
         private void Main_Load(object sender, EventArgs e) {
@@ -21,7 +26,7 @@ namespace Yearbook_Photos {
             try {
 
                 //connect to a main directory
-                string rootPath = @"C:\LocalStorage\static";
+                string rootPath = ROOT_PATH;
 
                 //the main directory has directories for every year
                 string[] yearFolders = Directory.GetDirectories(rootPath);
@@ -41,47 +46,30 @@ namespace Yearbook_Photos {
                         foreach (string csv in linkFiles) {
                             StreamReader reader = new StreamReader(csv);
                             while (!reader.EndOfStream) {
-                                string line = reader.ReadLine();
                                 //CSV columns: StudentID, PictureFile, FirstName, Grade, LastName, StudentID, Homeroom#
                                 //example: "1234","1234.jpg","CLARK","10","JAMES","1234","107"
-                                string[] columns = line.Split(',');
-                                ListViewItem item = new ListViewItem(columns[2].Replace("\"", string.Empty));
-                                item.SubItems.Add(columns[4].Replace("\"", string.Empty));
-                                item.SubItems.Add(columns[3].Replace("\"", string.Empty));
+                                string line = reader.ReadLine();
+                                string[] values = line.Split(',');
+                                //populate list view based on master record: list view: lastname, firstname, grade, id
+                                ListViewItem item = new ListViewItem(values[4].Replace("\"", string.Empty)); //last name
+                                item.SubItems.Add(values[2].Replace("\"", string.Empty)); //first name
+                                item.SubItems.Add(values[3].Replace("\"", string.Empty)); //grade
+                                item.SubItems.Add(values[0].Replace("\"", string.Empty)); //id
+                                item.ImageKey = subFolder;
                                 lvCurrent.Items.Add(item);
                             }
                         }
                     }
                 }
+                lvCurrent.Sort();
 
-                //populate list view based on master record: list view: lastname, firstname, grade
+                //show initial picture
+                lvCurrent.Items[0].Selected = true;
 
                 //catch exception
-            } catch {
-
+            } catch (Exception ex) {
+                lblStub.Text = ex.Message;
             }
-
-            //some additional events to handle:
-
-                //search
-                
-                //show selected picture
-
-                //export
-
-                //sort by list view column
-
-            /*
-            string directory = @"C:\Users\jwclark\Desktop\New folder";
-            string[] files = Directory.GetFiles(directory, "*.jpg");
-
-            
-            foreach (string path in files) {
-                ListViewItem listItem = new ListViewItem(
-                lvCurrent.Items.Add(key);
-                Image image = Image.FromFile(path);
-            }
-             * */
         }
         private void SetDropDownOptions(string rootPath, string[] yearFolders) {
             foreach (string s in yearFolders) {
@@ -91,20 +79,93 @@ namespace Yearbook_Photos {
             //sort top of list by most recently modified directory
             if (comboYear.Items.Count > 0) {
                 DateTime newest = new DateTime();
-                foreach (string s in yearFolders) {
-                    DateTime creationTime = Directory.GetCreationTime(s);
+                int count = 0;
+                foreach (string folder in yearFolders) {
+                    DateTime creationTime = Directory.GetCreationTime(folder);
                     if (creationTime > newest) {
-                        comboYear.SelectedIndex = 0;
+                        newest = creationTime;
+                        comboYear.SelectedIndex = count;
+                        count++;
                     }
                 }
             }
         }
-        /*
-        private void lvCurrent_SelectedIndexChanged(object sender, EventArgs e) {
-            string path = lvCurrent.SelectedItems[0].Text;
-            Image image = Image.FromFile(path);
-            pnlImage.BackgroundImage = image;
+
+        private void lvCurrent_ColumnClick(object sender, ColumnClickEventArgs e) {
+            if (e.Column == sorter.SortColumn) {
+                if (sorter.Order == SortOrder.Ascending) {
+                    sorter.Order = SortOrder.Descending;
+                } else {
+                    sorter.Order = SortOrder.Ascending;
+                }
+            } else {
+                sorter.SortColumn = e.Column;
+                sorter.Order = SortOrder.Ascending;
+            }
+            lvCurrent.Sort();
         }
-         * */
+        private void lvCurrent_SelectedIndexChanged(object sender, EventArgs e) {
+            try {
+                string path = lvCurrent.SelectedItems[0].ImageKey + @"\"
+                    + lvCurrent.SelectedItems[0].SubItems[3].Text + ".jpg";
+                Image image = Image.FromFile(path);
+                pnlImage.Image = image;
+            } catch {
+
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) {
+            ListView.SelectedListViewItemCollection items = lvCurrent.SelectedItems;
+            
+
+            foreach (ListViewItem item in lvCurrent.SelectedItems) {
+                lvCurrent.Items.Remove(item);
+                lvExport.Items.Add(item);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e) {
+            foreach (ListViewItem item in lvExport.SelectedItems) {
+                lvExport.Items.Remove(item);
+                lvCurrent.Items.Add(item);
+            }
+            lvCurrent.Sort();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e) {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            ProgressBar progress = new ProgressBar();
+            
+            foreach (ListViewItem item in lvExport.Items) {
+                string source = item.ImageKey + @"\" + item.SubItems[3].Text + ".jpg";
+                string destination = fbd.SelectedPath + @"\" + item.SubItems[3].Text + ".jpg";
+                lblStub.Text = "source: " + source + " , destination: " + destination;
+                File.Copy(source, destination);
+            }
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar == (char)13) {
+                e.Handled = true;
+                lvCurrent.Focus();
+                foreach (ListViewItem item in lvCurrent.SelectedItems) {
+                    item.Selected = false;
+                }
+                try {
+                    lvCurrent.FindItemWithText(txtSearch.Text).Selected = true;
+                } catch (NullReferenceException) {
+                    lblSearchMessage.Text = "A match was not found.";
+                }
+            }
+        }
+
+        private void txtSearch_Click(object sender, EventArgs e) {
+            lblSearchMessage.Text = string.Empty;
+            txtSearch.SelectionStart = 0;
+            txtSearch.SelectionLength = txtSearch.Text.Length;
+        }
     }
 }
