84f42127dec288d27c4c2cf437e5d078af9ac3ba
JamesWClark <jwclark@rockhursths.edu>
2014-05-25 00:39:23 -0500

    successfully implemented a date picker

--- CalendarFeeds/Athletics/rhs-sports-calendar-feed.js
+++ CalendarFeeds/Athletics/rhs-sports-calendar-feed.js
@@ -9,16 +9,19 @@
  *
  * A sample of HTML that uses this code:
  *
- * <div id="calendar-feed"></div>
+ * <div id="calendar-feed-select"></div>
+ * <div id="calendar-feed">
+ *     <select id="calendar-feed-select" onchange="updateCalendar(this);"></select>
+ * </div>
  * <script src="https://www.rockhursths.edu/file/admin---webdev-documents/moment.min.js"></script>
  * <script src="https://www.rockhursths.edu/file/admin---webdev-documents/jw-clark---web-assets/rhs-sports-calendar-feed.js"></script>
  * <script>
  * 
  *     var calendarId = 'rockhursths.edu_fpdevts40susg3j1ueoubsrrt8@group.calendar.google.com'; //get this value from calendar settings
- *     var startMin = '2013-11-01'; //YYYY-MM-DD  <-- mandatory format
- *     var startMax = '2014-04-01'; //YYYY-MM-DD  <-- mandatory format
+ *     var earliestYear = 2013; //the first year Google Calendar was implemented for this organization
+ *     var season = 'Fall'; //other options: Spring, Winter, Summer
  * 
- *     $(document).ready(getCalendar(calendarId, startMin, startMax));
+ *     $(document).ready(init());
  * 
  * </script>
  * <noscript>
@@ -28,12 +31,15 @@
 
 var maxResults = 100;
 var timePlaceholder = '5:55 pm'; //any time that matches this will print as TBD
-var unknownLocation = '???'; //if this is encountered in the location field, the hyperlink will be removed
+var unknown_location_placeholders_array = ['?','??','???','????','?????','tbd','TBD','unknown']; //if any of these are encountered in the location field, the hyperlink will be removed
+var rolloverDate = "05-21"; //format as MM-DD, when the year rolls over on this MM-DD date, the calendars pivot for client display
+var startMin;
+var startMax;
 
 /** called by document.ready */
-function getCalendar(calendarId, startMin, startMax) {
+function getCalendar(calendarId) {
     var https = 'https://www.google.com/calendar/feeds/' + calendarId
-    + '/public/full?alt=json&max-results=' + maxResults + '&orderby=starttime&sortorder=ascending&singleevents=true&start-min=' + startMin + '&start-max=' + startMax;
+        + '/public/full?alt=json&max-results=' + maxResults + '&orderby=starttime&sortorder=ascending&singleevents=true&start-min=' + startMin + '&start-max=' + startMax;
     $.ajax({
         url: https,
         dataType: 'jsonp',
@@ -42,6 +48,60 @@ function getCalendar(calendarId, startMin, startMax) {
             printFeed(response.feed);
         }
     });
+}
+/** called one time by $(document).ready() */
+function init() {
+    var calStartYear; //display range start
+    var calFinishYear; //display range finish
+    var currentYear = parseInt(moment().format("YYYY")); //at time of client visit
+    var now = moment(); //at time of client visit
+    var start = moment(currentYear + "-" + rolloverDate); //rollover start
+    var finish = moment(start).add('years', 1); //rollover finish
+    var now_month_is_less_than_start_month = parseInt(moment(now).format('MM')) < parseInt(moment(start).format('MM'));
+
+    if (now_month_is_less_than_start_month) {
+        calStartYear = currentYear - 1;
+        calFinishYear = currentYear;
+    } else {
+        calStartYear = currentYear;
+        calFinishYear = currentYear + 1;
+    }
+
+    setMinMax(calStartYear, calFinishYear);
+    setSelectOptions(calStartYear, calFinishYear);
+    getCalendar(calendarId);
+}
+/** one time call by setCurrent()
+ *  multiple calls for every select option change event
+ */
+function setMinMax(calStartYear, calFinishYear) {
+    switch (season.toLowerCase()) {
+        case "winter":
+            startMin = calStartYear + "-11-01";
+            startMax = calFinishYear + "-04-01";
+            break;
+        case "spring":
+            startMin = calStartYear + "-01-01";
+            startMax = calStartYear + "-06-01";
+            break;
+        case "fall":
+            startMin = calStartYear + "-08-01";
+            startMax = calStartYear + "-12-31";
+            break;
+    }
+}
+/** called by setCurrent */
+function setSelectOptions(calStartYear, calFinishYear) {
+    var list = $('#calendar-feed-select');
+    var counter = calStartYear;
+    while (counter > earliestYear-1) {
+        var text = counter + "-" + (1+counter).toString().slice(-2);
+        var value = counter--;
+        $(list).append($('<option>', {
+            value: value,
+            text: text
+        }));
+    }
 }
 /** called by printFeed */
 function setGoogleCalendarButton(calendarId) {
@@ -65,7 +125,7 @@ function printFeed(feed) {
         html += '<td><a href="' + link + '" target="_blank">' + title + '</a></td>';
         html += '<td>' + moment(start).format('MMM DD') + '</td>';
         html += '<td>' + evalTBD(start) + '</td>';
-        if (location === unknownLocation)
+        if (unknown_location_placeholders_array.indexOf(location) > -1) //don't show map if text matches the unknownlocations list
             html += '<td>' + location + '</td>';
         else
             html += '<td><a href="https://maps.google.com/maps?q=' + location + '&hl=en" target="_blank">' + location + ' </a></td>';
@@ -73,10 +133,9 @@ function printFeed(feed) {
         if (description.trim().length > 0)
             html += '<tr><td colspan="4">' + description + '</td></tr>';
         html += '<tr class="rhs-sports-calendar-feed-table-row-spacer"><td colspan="4"></td></tr>';
-
     }
     html += '</table><br />';
-    $('#calendar-feed').append(html);
+    $('#calendar-feed').html(html);
     setGoogleCalendarButton(calendarId);
 }
 /** called by printFeed */
@@ -86,4 +145,9 @@ function evalTBD(time) {
         return 'TBD';
     else
         return t;
+}
+/** called by #calendar-feed-select onchange event */
+function updateCalendar(option) {
+    setMinMax(option.value, 1 + parseInt(option.value));
+    getCalendar(calendarId);
 }
