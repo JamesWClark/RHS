0ce730c661d447946fc09da7d619821c34673837
JamesWClark <jwclark@rockhursths.edu>
2016-08-16 09:17:28 -0500

    Add modal dialog to display people description on click

--- HallOfFame/halloffame.js
+++ HallOfFame/halloffame.js
@@ -1,32 +1,46 @@
 // https://docs.google.com/spreadsheets/d/1l6FPkGBlofyTwfysLFIcdj_oRwmggnKn5tVWjnXxwy4/edit#gid=1691578779
 
-var log = function(msg) {
+var log = function (msg) {
     console.log(msg);
 };
 
-$(document).ready(function() {
-    $.get('https://spreadsheets.google.com/feeds/list/1l6FPkGBlofyTwfysLFIcdj_oRwmggnKn5tVWjnXxwy4/1/public/values?alt=json', function(data) {
-        
+$(document).ready(function () {
+    $.get('https://spreadsheets.google.com/feeds/list/1l6FPkGBlofyTwfysLFIcdj_oRwmggnKn5tVWjnXxwy4/1/public/values?alt=json', function (data) {
+
         // store the hall of famers in a dictionary
-        var people = {}; 
-        
+        var people = {};
+
         // parse the cells from google spreadsheet
-        data.feed.entry.forEach(function(entry) {
+        data.feed.entry.forEach(function (entry) {
             var name = entry.gsx$name.$t;
-            var desc = entry.gsx$description.$t;            
-            if(desc.length > 0) {
+            var desc = entry.gsx$description.$t;
+            if (desc.length > 0) {
                 people[name] = desc;
             }
         });
-        
+
         // find matching names in the table
-        $('td').each(function(index, html) {
+        $('td').each(function (index, html) {
             var name = $(this).text();
             if (people.hasOwnProperty(name)) {
-               $(this).wrapInner('<a href="" data-name="' + name + '"></a>');
+                $(this).addClass('rhs-hall-of-fame-link');
+                $(this).click(function () {
+                    var desc = people[name];
+                    $('<div></div>')
+                    .text(desc)
+                    .dialog({
+                        modal: true,
+                        title: 'Description',
+                        buttons: {
+                            Ok: function() {
+                                $(this).dialog('destroy').remove();
+                            }
+                        }
+                    });
+                });
             }
         });
-        
+
         // TODO: http://jsfiddle.net/06nLLtbb/1/
         // and also - stop using <a> tags and just click the element...
     });
