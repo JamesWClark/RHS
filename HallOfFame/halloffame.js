9fdd36b9f8350b3c3627f56ba6e95be44e4d4946
J.W. Clark <jwclark@rockhursths.edu>
2017-01-18 00:29:37 -0600

    Separate dom maintenance from this problem. Relies mostly on script and css.

--- HallOfFame/halloffame.js
+++ HallOfFame/halloffame.js
@@ -1,47 +1,63 @@
-// https://docs.google.com/spreadsheets/d/1l6FPkGBlofyTwfysLFIcdj_oRwmggnKn5tVWjnXxwy4/edit#gid=1691578779
-
-var log = function (msg) {
-    console.log(msg);
-};
+/**
+ * Rockhurst High School Alumni Hall of Fame 
+ *
+ * This script grabs the inductees from a Google Sheet and displays them along with modal descriptions
+ *
+ * https://docs.google.com/spreadsheets/d/1l6FPkGBlofyTwfysLFIcdj_oRwmggnKn5tVWjnXxwy4/edit#gid=701759400
+ * Permitted Authors: Robbie Haden, J.W. Clark
+ */
 
 $(document).ready(function () {
-    $.get('https://spreadsheets.google.com/feeds/list/1l6FPkGBlofyTwfysLFIcdj_oRwmggnKn5tVWjnXxwy4/1/public/values?alt=json', function (data) {
+    
+    var log = function (msg) {
+        console.log(msg);
+    };
+    
+    // http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+    String.prototype.hashCode = function(){
+        var hash = 0;
+        if (this.length == 0) return hash;
+        for (var i = 0; i < this.length; i++) {
+            var c = this.charCodeAt(i);
+            hash = ((hash<<5)-hash)+c;
+            hash = hash & hash; // Convert to 32bit integer
+        }
+        return hash;
+    }
 
-        // store the hall of famers in a dictionary
-        var people = {};
+    $.get('https://spreadsheets.google.com/feeds/list/1l6FPkGBlofyTwfysLFIcdj_oRwmggnKn5tVWjnXxwy4/oblt4s2/public/values?alt=json', function (data) {
 
-        // parse the cells from google spreadsheet
+        var modals = $('#modals');
+        var inductees = $('#hall-of-fame-inductees');
+        
+        
+        // iterate the cells from inductees worksheet
         data.feed.entry.forEach(function (entry) {
-            var name = entry.gsx$name.$t;
-            var desc = entry.gsx$description.$t;
-            if (desc.length > 0) {
-                people[name] = desc;
+            // raw data
+            var firstName      = entry.gsx$firstname.$t;
+            var lastName       = entry.gsx$lastname.$t;
+            var middleName     = entry.gsx$middlename.$t;
+            var graduationYear = entry.gsx$graduationyear.$t;
+            var inductionYear  = entry.gsx$inductionyear.$t;
+            var description    = entry.gsx$description.$t;
+
+            // prepped data
+            if(graduationYear.length > 0) graduationYear = '\'' + graduationYear.substring(2);
+            if(inductionYear.length > 0) inductionYear = '(' + inductionYear + ')';
+            
+            // the list item to display
+            var display = firstName + ' ' + middleName + ' ' + lastName + ' ' + graduationYear + ' ' + inductionYear;
+            var key = 'h' + display.hashCode();
+            
+            if(description.length > 0) {
+                inductees.append('<li><a href="#' + key + '" rel="modal:open">' + display + '</a></li>');                
+                modals.append('<div id="' + key + '" style="display:none;"><h3>' + display + '</h3><hr><p>' + description + '</p></div>')
+            } else {
+                inductees.append('<li>' + display + '</li>');
             }
         });
-
-        // find matching names in the table
-        $('td').each(function (index, html) {
-            var name = $(this).text();
-            if (people.hasOwnProperty(name)) {
-                $(this).addClass('rhs-hall-of-fame-link');
-                $(this).click(function () {
-                    var desc = people[name];
-                    $('<div></div>')
-                    .text(desc)
-                    .dialog({
-                        modal: true,
-                        title: 'Description',
-                        buttons: {
-                            Ok: function() {
-                                $(this).dialog('destroy').remove();
-                            }
-                        }
-                    });
-                });
-            }
-        });
-
-        // TODO: http://jsfiddle.net/06nLLtbb/1/
-        // and also - stop using <a> tags and just click the element...
+        
+        // iterate cells from friends worksheet
+        
     });
 });
