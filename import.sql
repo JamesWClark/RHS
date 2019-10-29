0318b5a32ac831ed5b4908d3ba3c00f9391b47aa
J.W. Clark <jwclark@rockhursths.edu>
2019-10-28 21:09:24 -0500

    Update import.sql

--- import.sql
+++ import.sql
@@ -2,3 +2,9 @@
 
 SELECT Core.[User ID] AS user_id, LT.[Image Name] AS filename
 FROM Core INNER JOIN LT ON Core.[Student ID] = LT.[Student ID];
+
+-- LT Headers
+-- Student ID	Image Name	First Name	Grade	Last Name	Patron #	External Student ID
+
+-- Core Headers
+-- User ID	Student ID	First Name	Nickname	Last Name	Grad Year	E-Mail
